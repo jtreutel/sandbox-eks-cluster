90971217809b2e455e8b4ad24b9f6481128cba51
jtreutel <jennings@circleci.com>
2021-03-26 12:59:42 +0900

    Reorganize files for convenience.

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "education-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+
 # Kubernetes provider
 # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
 # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
